035cdd45d118487f4981944cddefbe8455afa66d
CodedDeveloper117 <coded.developer098@gmail.com>
2022-06-26 16:09:15 +0100

    Started

--- app/src/main/java/com/cd/sdabibleapp/presentation/MainActivity.kt
+++ app/src/main/java/com/cd/sdabibleapp/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cd.sdabibleapp.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,16 +9,25 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.cd.sdabibleapp.ui.theme.SDABibleAppTheme
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SDABibleAppTheme {
-                // A surface container using the 'background' color from the theme
+                val viewModel: MainViewModel = hiltViewModel()
+                val state = viewModel.state.collectAsState()
+                LaunchedEffect(key1 = true) {
+                    Log.d(TAG, "onCreate: value: ${state.value}")
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
